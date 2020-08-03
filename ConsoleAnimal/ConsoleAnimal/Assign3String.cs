dedb5b9e925ceb206616335527c3ae56886e0f67
sobhandip1 <sobhandip.samanta@zensar.com>
2020-08-03 17:51:18 +0530

    Adding assignment3C#

---
+++ ConsoleAnimal/ConsoleAnimal/Assign3String.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAnimal
+{
+    class Assign3String
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter First String");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Enter Second String");
+            string s2 = Console.ReadLine();
+            int opt;
+            opt = Convert.ToInt32(Console.ReadLine());
+            switch (opt)
+            {
+                case 1:
+                    Console.WriteLine("Concate the string:");
+                    Console.WriteLine(string.Concat(s1, s2));
+                    break;
+
+                case 2:
+                    Console.WriteLine("Length of the string:");
+                    Console.WriteLine("length of the string is {0} and {1}", s1.Length, s2.Length);
+                    break;
+
+                case 3:
+                    Console.WriteLine("Sub string the strings:");
+                    Console.WriteLine(s1.Substring(0, 8));
+                    Console.WriteLine(s2.Substring(0, 6));
+                    break;
+                case 4:
+                    Console.WriteLine("Replace the string:");
+                    Console.WriteLine(s1.Replace(s1, "My name is"));
+                    break;
+                case 5:
+                    Console.WriteLine("uppercase the string:");
+                    Console.WriteLine(s1.ToUpper());
+                    break;
+                case 6:
+                    Console.WriteLine("Lower case the string:");
+                    Console.WriteLine(s2.ToLower());
+                    break;
+                case 7:
+                    Console.WriteLine("trim  the string:");
+                    Console.WriteLine(s1.Trim());
+                    break;
+                case 8:
+                    Console.WriteLine("Concate the string:");
+                    Console.WriteLine(s1.Split(' '));
+                    break;
+                default:
+                    Console.Write("Input correct option\n");
+                    break;
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleAnimal/ConsoleAnimal/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAnimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] MyArray;
+            MyArray = new string[5] { "Zebra", "Ant", "Owl", "Lion", "Monkey" };
+
+            Console.WriteLine("---For Loop---");
+
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(" " + MyArray[i]);
+
+
+            Console.WriteLine("-----Foreach Loop-----");
+
+            foreach (string x in MyArray)
+            {
+                Console.WriteLine(" " + x);
+            }
+
+
+            Console.WriteLine("------Do While Loop----");
+
+
+            int k = 0;
+            do
+            {
+                Console.WriteLine(" " + MyArray[k]);
+                k++;
+            } while (k < 5);
+
+            Console.WriteLine("----While Loop----");
+
+            int p = 0;
+            while (p < 5)
+            {
+                Console.WriteLine(" " +  MyArray[p]);
+                p++;
+            }
+
+            String[] animals;
+            animals = new string[] { "Zebra", "Kiwi", "Amazona Parrot", "Tiger" };
+            foreach (string s in animals)
+            {
+                Console.WriteLine(" " + s);
+            }
+            String[] places;
+            places = new string[] { "Africa", "New Zealand", "Jamaica", "India" };
+            foreach (string q in animals)
+            {
+                Console.WriteLine(" " + q);
+            }
+            Console.WriteLine();
+          Console.WriteLine("\nAfter Sort:");
+             Array.Sort(MyArray);
+
+            foreach (string g in MyArray)
+            {
+                Console.WriteLine(g);
+                // display sorted array 
+            }
+            Console.WriteLine("\nAfter Sort of animals array:");
+            Array.Sort(animals);
+
+            foreach (string g in animals)
+            {
+                Console.WriteLine(g);
+                // display sorted array 
+            }
+            Console.WriteLine("\nAfter Sort of place array:");
+            Array.Sort(places);
+
+            foreach (string g in places)
+            {
+                Console.WriteLine(g);
+                // display sorted array 
+            }
+            Console.Read();
+        }
+    }
+}
